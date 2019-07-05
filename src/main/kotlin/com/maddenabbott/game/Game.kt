751a91a7f9fd2aebec9305769cd95e36ad2222c0
Rupert Madden-Abbott <rupert@madden-abbott.com>
2019-07-05 13:55:57 +0100

    Refactor.

--- src/main/kotlin/com/maddenabbott/game/Game.kt
+++ src/main/kotlin/com/maddenabbott/game/Game.kt
@@ -4,11 +4,7 @@ import com.almasb.fxgl.app.GameApplication
 import com.almasb.fxgl.app.GameSettings
 import com.almasb.fxgl.core.math.Vec2
 import com.almasb.fxgl.dsl.entityBuilder
-import com.almasb.fxgl.dsl.getGameWorld
-import com.almasb.fxgl.entity.Entity
 import com.almasb.fxgl.entity.EntityFactory
-import com.almasb.fxgl.entity.SpawnData
-import com.almasb.fxgl.entity.Spawns
 import com.maddenabbott.game.framework.ceil
 import com.maddenabbott.game.framework.isEven
 import com.maddenabbott.game.framework.isOdd
@@ -19,7 +15,8 @@ import javafx.scene.shape.Rectangle
 
 class Square(
   val point: Vec2,
-  val color: Color
+  val color: Color,
+  val parent: Board
 )
 
 class Board(
@@ -37,26 +34,31 @@ class Board(
       Color.WHITE
     }
 
-    Square(Vec2(column.toDouble(), row.toDouble()), color)
+    Square(Vec2(column.toDouble(), row.toDouble()), color, this)
   }
 }
 
-enum class EntityType {
-  SQUARE
-}
-
 class GameEntityFactory(private val size: Dimension2D) : EntityFactory {
-  @Spawns("SQUARE")
-  fun newSqaure(data: SpawnData): Entity {
-    return entityBuilder()
-      .from(data)
-      .view(Rectangle(data.get("squareLength"), data.get("squareLength"), data.get("color")))
-      .build()
+  fun addSquare(square: Square) {
+    val squareLength = if (size.ratio() > square.parent.size.ratio()) {
+      size.width / square.parent.size.width
+    } else {
+      size.height / square.parent.size.height
+    }
+
+    val point = Vec2((square.point.x - 1) * squareLength, (square.point.y - 1) * squareLength)
+
+    entityBuilder()
+      .at(point)
+      .view(Rectangle(squareLength, squareLength, square.color))
+      .buildAndAttach()
   }
 }
 
 class Game : GameApplication() {
   private val size = Dimension2D(800.0, 800.0)
+
+  private val gameEntityFactory = GameEntityFactory(size)
 
   override fun initSettings(settings: GameSettings) {
     settings.width = size.width.toInt()
@@ -65,22 +67,7 @@ class Game : GameApplication() {
   }
 
   override fun initGame() {
-    getGameWorld().addEntityFactory(GameEntityFactory(size))
-
-    val board = Board(8, 8)
-
-    board.squares.forEach { square ->
-      val squareLength = if (size.ratio() > board.size.ratio()) {
-        size.width / board.size.width
-      } else {
-        size.height / board.size.height
-      }
-
-      val point = Vec2((square.point.x - 1) * squareLength, (square.point.y - 1) * squareLength)
-
-      getGameWorld().spawn(EntityType.SQUARE.name, SpawnData(point.toPoint2D()).put("squareLength", squareLength)
-        .put("color", square.color))
-    }
+    Board(8, 8).squares.forEach(gameEntityFactory::addSquare)
   }
 }
 
