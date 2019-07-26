7962643af8dcf06c9aecf6bc5046dbeeacdf53fd
Rupert Madden-Abbott <rupert@madden-abbott.com>
2019-07-27 00:35:33 +0100

    Add a win condition.

--- src/main/kotlin/com/maddenabbott/game/Game.kt
+++ src/main/kotlin/com/maddenabbott/game/Game.kt
@@ -110,7 +110,7 @@ class Board(
   columns: Int,
   rows: Int,
   width: Float,
-  texture: Texture
+  private val texture: Texture
 ) {
   private val squareWidth = width / columns
 
@@ -124,6 +124,10 @@ class Board(
 
   private var currentPlayer = Player.WHITE
 
+  var winningPlayer: Player? = null
+
+  var pieces = mutableMapOf<Square, Piece>()
+
   val squares = (1..(rows * columns)).map {
     val column = (it / rows.toDouble()).ceil()
     val row = it - (rows * (column - 1))
@@ -136,44 +140,60 @@ class Board(
     Square(row, column, squareWidth, color)
   }
 
-  val pieces = mutableMapOf(
-    get(1, 1) to Piece(PieceType.ROOK, Player.WHITE, texture),
-    get(2, 1) to Piece(PieceType.KNIGHT, Player.WHITE, texture),
-    get(3, 1) to Piece(PieceType.BISHOP, Player.WHITE, texture),
-    get(4, 1) to Piece(PieceType.QUEEN, Player.WHITE, texture),
-    get(5, 1) to Piece(PieceType.KING, Player.WHITE, texture),
-    get(6, 1) to Piece(PieceType.BISHOP, Player.WHITE, texture),
-    get(7, 1) to Piece(PieceType.KNIGHT, Player.WHITE, texture),
-    get(8, 1) to Piece(PieceType.ROOK, Player.WHITE, texture),
-    get(1, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
-    get(2, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
-    get(3, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
-    get(4, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
-    get(5, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
-    get(6, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
-    get(7, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
-    get(8, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
-    get(1, 8) to Piece(PieceType.ROOK, Player.BLACK, texture),
-    get(2, 8) to Piece(PieceType.KNIGHT, Player.BLACK, texture),
-    get(3, 8) to Piece(PieceType.BISHOP, Player.BLACK, texture),
-    get(4, 8) to Piece(PieceType.QUEEN, Player.BLACK, texture),
-    get(5, 8) to Piece(PieceType.KING, Player.BLACK, texture),
-    get(6, 8) to Piece(PieceType.BISHOP, Player.BLACK, texture),
-    get(7, 8) to Piece(PieceType.KNIGHT, Player.BLACK, texture),
-    get(8, 8) to Piece(PieceType.ROOK, Player.BLACK, texture),
-    get(1, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
-    get(2, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
-    get(3, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
-    get(4, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
-    get(5, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
-    get(6, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
-    get(7, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
-    get(8, 7) to Piece(PieceType.PAWN, Player.BLACK, texture)
-  )
+  init {
+    reset()
+  }
+
+  fun reset() {
+    selectedSquare = null
+    enPassantSquare = null
+    enPassantPieceSquare = null
+    currentPlayer = Player.WHITE
+    winningPlayer = null
+    pieces = mutableMapOf(
+      get(1, 1) to Piece(PieceType.ROOK, Player.WHITE, texture),
+      get(2, 1) to Piece(PieceType.KNIGHT, Player.WHITE, texture),
+      get(3, 1) to Piece(PieceType.BISHOP, Player.WHITE, texture),
+      get(4, 1) to Piece(PieceType.QUEEN, Player.WHITE, texture),
+      get(5, 1) to Piece(PieceType.KING, Player.WHITE, texture),
+      get(6, 1) to Piece(PieceType.BISHOP, Player.WHITE, texture),
+      get(7, 1) to Piece(PieceType.KNIGHT, Player.WHITE, texture),
+      get(8, 1) to Piece(PieceType.ROOK, Player.WHITE, texture),
+      get(1, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
+      get(2, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
+      get(3, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
+      get(4, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
+      get(5, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
+      get(6, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
+      get(7, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
+      get(8, 2) to Piece(PieceType.PAWN, Player.WHITE, texture),
+      get(1, 8) to Piece(PieceType.ROOK, Player.BLACK, texture),
+      get(2, 8) to Piece(PieceType.KNIGHT, Player.BLACK, texture),
+      get(3, 8) to Piece(PieceType.BISHOP, Player.BLACK, texture),
+      get(4, 8) to Piece(PieceType.QUEEN, Player.BLACK, texture),
+      get(5, 8) to Piece(PieceType.KING, Player.BLACK, texture),
+      get(6, 8) to Piece(PieceType.BISHOP, Player.BLACK, texture),
+      get(7, 8) to Piece(PieceType.KNIGHT, Player.BLACK, texture),
+      get(8, 8) to Piece(PieceType.ROOK, Player.BLACK, texture),
+      get(1, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
+      get(2, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
+      get(3, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
+      get(4, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
+      get(5, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
+      get(6, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
+      get(7, 7) to Piece(PieceType.PAWN, Player.BLACK, texture),
+      get(8, 7) to Piece(PieceType.PAWN, Player.BLACK, texture)
+    )
+  }
 
   fun get(column: Int, row: Int) = squares.find { it.column == column && it.row == row }!!
 
   fun touch(x: Float, y: Float) {
+    if (winningPlayer != null) {
+      reset()
+      return
+    }
+
     val touchedSquare = get(ceil(x / squareWidth), ceil(y / squareWidth))
     val selectedPiece = pieces[selectedSquare]
 
@@ -209,6 +229,12 @@ class Board(
       pieces[touchedSquare] = selectedPiece
       selectedSquare = null
       currentPlayer = currentPlayer.next()
+
+      if(pieces.filter { it.value.owner == currentPlayer }
+        .flatMap { (square, piece) -> calculateValidMoveSquares(square, piece, pieces) }
+        .isEmpty()) {
+        winningPlayer = currentPlayer.next()
+      }
     }
 
     validMoveSquares.clear()
@@ -330,9 +356,34 @@ class GameEntityFactory(
       )
     }
   }
+
+  fun add(winningPlayer: Player, width: Float, height: Float) {
+    val text = "Checkmate! $winningPlayer has won!"
+    font.data.setScale(3f)
+    glyphLayout.setText(font, text)
+
+    val fontX = (width - glyphLayout.width) / 2
+    val fontY = (height + glyphLayout.height) / 2
+    val padding = 30
+
+    batch.use {
+      shapeRenderer.use(ShapeRenderer.ShapeType.Filled) {
+        it.color = Color.WHITE
+        it.rect(fontX - padding / 2, fontY - (padding / 2) - glyphLayout.height, glyphLayout.width + padding, glyphLayout.height + padding)
+      }
+    }
+
+    batch.use {
+      font.color = Color.BLACK
+      font.draw(batch, text, fontX, fontY)
+    }
+  }
 }
 
-class GameScreen(width: Float, height: Float) : KtxScreen, KtxInputAdapter {
+class GameScreen(
+  private val width: Float,
+  private val height: Float
+) : KtxScreen, KtxInputAdapter {
   private val board = Board(8, 8, width, Texture(Gdx.files.internal("pieces.png")))
   private val factory = GameEntityFactory(BitmapFont(), SpriteBatch(), ShapeRenderer())
   private val camera = OrthographicCamera(width, height)
@@ -347,6 +398,7 @@ class GameScreen(width: Float, height: Float) : KtxScreen, KtxInputAdapter {
     camera.update()
     board.squares.forEach { square -> factory.add(board, square) }
     board.pieces.forEach { (square, piece) -> factory.add(square, piece) }
+    board.winningPlayer?.also { factory.add(it, width, height) }
   }
 
   override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
